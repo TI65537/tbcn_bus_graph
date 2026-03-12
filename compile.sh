f9713538d081c028b1fcab97d37e4b60d67fa2f4
TI65537 <162770132+TI65537@users.noreply.github.com>
2026-03-13 03:35:26 +0900

    Add files via upload

---
+++ compile.sh
@@ -0,0 +1,38 @@
+# !/bin/sh
+
+c++ tbcn_bus_graph_cpp_1.cpp -o tbcn_bus_graph_cpp_1.out
+./tbcn_bus_graph_cpp_1.out>tbcn_bus_graph_cpp_1.dot
+dot tbcn_bus_graph_cpp_1.dot -n1 -Tsvg -o tbcn_bus_graph_cpp_1.svg
+
+c++ tbcn_bus_graph_cpp_2.cpp -o tbcn_bus_graph_cpp_2.out
+./tbcn_bus_graph_cpp_2.out>tbcn_bus_graph_cpp_2.dot
+dot tbcn_bus_graph_cpp_2.dot -n1 -Tsvg -o tbcn_bus_graph_cpp_2.svg
+
+c++ tbcn_bus_graph_cpp_3.cpp -o tbcn_bus_graph_cpp_3.out
+./tbcn_bus_graph_cpp_3.out>tbcn_bus_graph_cpp_3.dot
+dot tbcn_bus_graph_cpp_3.dot -n1 -Tsvg -o tbcn_bus_graph_cpp_3.svg
+
+c++ tbcn_bus_graph_cpp_4.cpp -o tbcn_bus_graph_cpp_4.out
+./tbcn_bus_graph_cpp_4.out>tbcn_bus_graph_cpp_4.dot
+dot tbcn_bus_graph_cpp_4.dot -n1 -Tsvg -o tbcn_bus_graph_cpp_4.svg
+
+c++ tbcn_bus_graph_cpp_a.cpp -o tbcn_bus_graph_cpp_a.out
+./tbcn_bus_graph_cpp_a.out>tbcn_bus_graph_cpp_a.dot
+dot tbcn_bus_graph_cpp_a.dot -n1 -Tsvg -o tbcn_bus_graph_cpp_a.svg
+
+c++ tbcn_bus_graph_cpp_b.cpp -o tbcn_bus_graph_cpp_b.out
+./tbcn_bus_graph_cpp_b.out>tbcn_bus_graph_cpp_b.dot
+dot tbcn_bus_graph_cpp_b.dot -n1 -Tsvg -o tbcn_bus_graph_cpp_b.svg
+
+c++ tbcn_bus_graph_cpp_c.cpp -o tbcn_bus_graph_cpp_c.out
+./tbcn_bus_graph_cpp_c.out>tbcn_bus_graph_cpp_c.dot
+dot tbcn_bus_graph_cpp_c.dot -n1 -Tsvg -o tbcn_bus_graph_cpp_c.svg
+
+c++ tbcn_bus_graph_cpp_d.cpp -o tbcn_bus_graph_cpp_d.out
+./tbcn_bus_graph_cpp_d.out>tbcn_bus_graph_cpp_d.dot
+dot tbcn_bus_graph_cpp_d.dot -n1 -Tsvg -o tbcn_bus_graph_cpp_d.svg
+
+c++ tbcn_bus_graph_cpp_e.cpp -o tbcn_bus_graph_cpp_e.out
+./tbcn_bus_graph_cpp_e.out>tbcn_bus_graph_cpp_e.dot
+dot tbcn_bus_graph_cpp_e.dot -n1 -Tsvg -o tbcn_bus_graph_cpp_e.svg
+
